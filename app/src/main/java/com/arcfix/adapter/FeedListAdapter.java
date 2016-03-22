4ce77f80b0b26f1c204e4fd37812a5a8ebd5854c
Deep <deepchand@startxlabs.com>
2016-03-22 16:01:53 +0530

    - implement footer view

--- app/src/main/java/com/arcfix/adapter/FeedListAdapter.java
+++ app/src/main/java/com/arcfix/adapter/FeedListAdapter.java
@@ -1,6 +1,7 @@
 package com.arcfix.adapter;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,39 +16,66 @@ import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by deep on 21/03/16.
  */
-public class FeedListAdapter extends  RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class FeedListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-      private MainResponse data;
+    private MainResponse data;
     private View.OnClickListener onClick;
     private Context context;
-    public FeedListAdapter(Context context,View.OnClickListener onClick,MainResponse data){
-        this.context=context;
-        this.onClick=onClick;
-        this.data=data;
+    private static final int TYPE_ITEM = 1;
+    private static final int TYPE_FOOTER = 2;
+
+    public FeedListAdapter(Context context, View.OnClickListener onClick, MainResponse data) {
+        this.context = context;
+        this.onClick = onClick;
+        this.data = data;
     }
+
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(context).inflate(R.layout.row_feed_list,parent,false);
-        return new RowHolder(view);
+        if (viewType == TYPE_ITEM) {
+
+            View view = LayoutInflater.from(context).inflate(R.layout.row_feed_list, parent, false);
+            return new RowHolder(view);
+        } else if (viewType == TYPE_FOOTER) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_layout_footer, parent, false);
+            return new FooterHolder(v);
+        }
+        return null;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        Picasso.with(context).load(R.mipmap.dummy).into(((RowHolder)holder).imgBig);
-        ((RowHolder)holder).imgIconVideo.setVisibility(View.VISIBLE);
-        ((RowHolder)holder).txtHeading.setTextColor(context.getResources().getColor(R.color.white));
+        if (holder instanceof RowHolder) {
+            Picasso.with(context).load(R.mipmap.dummy).into(((RowHolder) holder).imgBig);
+            ((RowHolder) holder).imgIconVideo.setVisibility(View.VISIBLE);
+            ((RowHolder) holder).txtHeading.setTextColor(context.getResources().getColor(R.color.white));
+        } else if (holder instanceof FooterHolder) {
+
+        }
     }
+
 
     @Override
     public int getItemCount() {
-        return data!=null?data.getItems().size():7;
+        return data != null ? data.getItems().size()+1 : 7;
     }
 
-    public class RowHolder extends RecyclerView.ViewHolder{
+    @Override
+    public int getItemViewType(int position) {
+        if (data!=null?position < data.getItems().size():position<6) {
+            return TYPE_ITEM;
+        } else if (data!=null?position == data.getItems().size():position==6) {
+            return TYPE_FOOTER;
+        }
+        return TYPE_ITEM;
+    }
+
+    public class RowHolder extends RecyclerView.ViewHolder {
 
         @Bind(R.id.image_company_icon)
         ImageView imgCompanyLogo;
@@ -79,6 +107,62 @@ public class FeedListAdapter extends  RecyclerView.Adapter<RecyclerView.ViewHold
         public RowHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
+        }
+
+        @OnClick({})
+        void onViewClick(View view) {
+
+        }
+    }
+
+    public class FooterHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.card_load_more_view)
+        CardView cardViewMore;
+        @Bind(R.id.card_login_view)
+        CardView cardLoginView;
+
+        @Bind(R.id.card_update_view)
+        CardView cardUpdateView;
+
+        @Bind(R.id.lnr_dismiss_login)
+        LinearLayout lnrDismissLogin;
+
+        @Bind(R.id.lnr_login)
+        LinearLayout lnrLogin;
+
+        @Bind(R.id.lnr_dismiss_update)
+        LinearLayout lnrDismissUpdate;
+
+        @Bind(R.id.lnr_yes_update)
+        LinearLayout yesUpdateLnr;
+
+        public FooterHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        @OnClick({R.id.lnr_login, R.id.lnr_dismiss_update, R.id.lnr_dismiss_login, R.id.lnr_yes_update, R.id.card_load_more_view})
+        void onViewClick(View view) {
+            switch (view.getId()) {
+                case R.id.lnr_dismiss_login:
+
+                    cardLoginView.setVisibility(View.GONE);
+                    break;
+                case R.id.lnr_login:
+                    break;
+                case R.id.lnr_yes_update:
+                    break;
+                case R.id.lnr_dismiss_update:
+                    cardUpdateView.setVisibility(View.GONE);
+                    break;
+                case R.id.card_load_more_view:
+
+                    //TODO: call API
+                    cardViewMore.setVisibility(View.GONE);
+                    break;
+
+            }
         }
     }
 }
--- app/src/main/java/com/arcfix/fragment/FeedsFragment.java
+++ app/src/main/java/com/arcfix/fragment/FeedsFragment.java
@@ -1,9 +1,11 @@
 package com.arcfix.fragment;
 
+import android.graphics.drawable.NinePatchDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -14,7 +16,13 @@ import android.widget.ProgressBar;
 import com.arcfix.R;
 import com.arcfix.adapter.FeedListAdapter;
 import com.arcfix.rest_api.APIClient;
+import com.arcfix.rest_api.data_model.Item;
 import com.arcfix.rest_api.data_model.MainResponse;
+import com.h6ah4i.android.widget.advrecyclerview.draggable.RecyclerViewDragDropManager;
+import com.h6ah4i.android.widget.advrecyclerview.swipeable.RecyclerViewSwipeManager;
+import com.h6ah4i.android.widget.advrecyclerview.touchguard.RecyclerViewTouchActionGuardManager;
+
+import java.util.HashMap;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -36,7 +44,12 @@ public class FeedsFragment extends Fragment {
     ProgressBar mProgressBar;
     private MainResponse data;
     private FeedListAdapter mAdapter;
-
+    private RecyclerView.Adapter mWrappedAdapter;
+    private RecyclerViewDragDropManager mRecyclerViewDragDropManager;
+    private RecyclerViewSwipeManager mRecyclerViewSwipeManager;
+    private RecyclerViewTouchActionGuardManager mRecyclerViewTouchActionGuardManager;
+private int lastRemoved=-1;
+    private Item mItemRemoved;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -52,6 +65,73 @@ public class FeedsFragment extends Fragment {
         return feedView;
     }
 
+//void setSwipeDrag(){
+//
+//    // touch guard manager  (this class is required to suppress scrolling while swipe-dismiss animation is running)
+//    mRecyclerViewTouchActionGuardManager = new RecyclerViewTouchActionGuardManager();
+//    mRecyclerViewTouchActionGuardManager.setInterceptVerticalScrollingWhileAnimationRunning(true);
+//    mRecyclerViewTouchActionGuardManager.setEnabled(true);
+//
+//
+//    mRecyclerViewDragDropManager = new RecyclerViewDragDropManager();
+//
+////    mRecyclerViewDragDropManager.setDraggingItemShadowDrawable(
+////            (NinePatchDrawable) ContextCompat.getDrawable(getContext(), R.drawable.material_shadow_z3));
+//
+//    // swipe manager
+//    mRecyclerViewSwipeManager = new RecyclerViewSwipeManager();
+//
+//    //adapter
+//    final MyDraggableSwipeableItemAdapter myItemAdapter = new MyDraggableSwipeableItemAdapter(getDataProvider());
+//    myItemAdapter.setEventListener(new MyDraggableSwipeableItemAdapter.EventListener() {
+//        @Override
+//        public void onItemRemoved(int position) {
+//            ((DraggableSwipeableExampleActivity) getActivity()).onItemRemoved(position);
+//        }
+//
+//        @Override
+//        public void onItemPinned(int position) {
+//            ((DraggableSwipeableExampleActivity) getActivity()).onItemPinned(position);
+//        }
+//
+//        @Override
+//        public void onItemViewClicked(View v, boolean pinned) {
+//            onItemViewClick(v, pinned);
+//        }
+//    });
+//
+//    mAdapter = myItemAdapter;
+//
+//    mWrappedAdapter = mRecyclerViewDragDropManager.createWrappedAdapter(myItemAdapter);      // wrap for dragging
+//    mWrappedAdapter = mRecyclerViewSwipeManager.createWrappedAdapter(mWrappedAdapter);      // wrap for swiping
+//
+//    final GeneralItemAnimator animator = new SwipeDismissItemAnimator();
+//
+//    // Change animations are enabled by default since support-v7-recyclerview v22.
+//    // Disable the change animation in order to make turning back animation of swiped item works properly.
+//    animator.setSupportsChangeAnimations(false);
+//
+//    mRecyclerView.setLayoutManager(mLayoutManager);
+//    mRecyclerView.setAdapter(mWrappedAdapter);  // requires *wrapped* adapter
+//    mRecyclerView.setItemAnimator(animator);
+//
+//    // additional decorations
+//    //noinspection StatementWithEmptyBody
+//    if (supportsViewElevation()) {
+//        // Lollipop or later has native drop shadow feature. ItemShadowDecorator is not required.
+//    } else {
+//        mRecyclerView.addItemDecoration(new ItemShadowDecorator((NinePatchDrawable) ContextCompat.getDrawable(getContext(), R.drawable.material_shadow_z1)));
+//    }
+//    mRecyclerView.addItemDecoration(new SimpleListDividerDecorator(ContextCompat.getDrawable(getContext(), R.drawable.list_divider_h), true));
+//
+//    // NOTE:
+//    // The initialization order is very important! This order determines the priority of touch event handling.
+//    //
+//    // priority: TouchActionGuard > Swipe > DragAndDrop
+//    mRecyclerViewTouchActionGuardManager.attachRecyclerView(mRecyclerView);
+//    mRecyclerViewSwipeManager.attachRecyclerView(mRecyclerView);
+//    mRecyclerViewDragDropManager.attachRecyclerView(mRecyclerView);
+//}
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -100,4 +180,51 @@ public class FeedsFragment extends Fragment {
 
         }
     };
-}
+
+    /**
+     * This method will be called when a list item is removed
+     *
+     * @param position The position of the item within data set
+     */
+    public void onItemRemoved(int position) {
+        Snackbar snackbar = Snackbar.make(
+                getView(),
+                R.string.app_name,
+                Snackbar.LENGTH_LONG);
+
+        snackbar.setAction(R.string.undo, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onItemUndoActionClicked();
+            }
+        });
+        snackbar.setActionTextColor(ContextCompat.getColor(getActivity(), R.color.grey));
+        snackbar.show();
+    }
+
+
+    public void onItemPinned(int position) {
+
+    }
+
+
+    public void onItemClicked(int position) {
+           mAdapter.notifyItemChanged(position);
+        }
+
+    private void onItemRemovedActionClicked(int position,Item object) {
+
+        lastRemoved=position;
+        mItemRemoved=object;
+    }
+
+
+    private void onItemUndoActionClicked() {
+//        set last removed object in variable
+data.getItems().add(lastRemoved,mItemRemoved);
+           mAdapter.notifyItemInserted(lastRemoved);
+        }
+    }
+
+
+
